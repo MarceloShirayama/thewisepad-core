c8b5770a17f83c82a99d23120d7532ee0232c5b4
Marcelo Shirayama <caixadecorreiodomarcelo@gmail.com>
2022-07-12 19:03:17 -0300

    refactor: make frequently used test data to global variables in signup.spec

--- tests/use-cases/signup.spec.ts
+++ tests/use-cases/signup.spec.ts
@@ -5,24 +5,20 @@ import { Encoder } from '@/use-cases/signup/ports/encoder'
 import { InMemoryUserRepository } from './in-memory-user-repository'
 import { FakeEncoder } from './signup/fake-encoder'
 
+const validEmail = 'valid@mail.com'
+const validPassword = 'valid-password_1'
+const userSignupRequest: UserData = {
+  email: validEmail,
+  password: validPassword
+}
+const encoder: Encoder = new FakeEncoder()
+const userRepository: UserRepository = new InMemoryUserRepository([])
+
+const sut: Signup = new Signup(userRepository, encoder)
+
 describe('Signup use case', () => {
   it('Should signup user with valid data', async () => {
-    const validEmail = 'valid@mail.com'
-    const validPassword = 'valid-password_1'
-    const userSignupRequest: UserData = {
-      email: validEmail,
-      password: validPassword
-    }
-
-    const userRepository: UserRepository = new InMemoryUserRepository([])
-
-    const encoder: Encoder = new FakeEncoder()
-
-    const useCase: Signup = new Signup(userRepository, encoder)
-
-    const userSignupResponse: UserData = await useCase.perform(
-      userSignupRequest
-    )
+    const userSignupResponse: UserData = await sut.perform(userSignupRequest)
 
     expect(userSignupResponse).toEqual(userSignupRequest)
     expect((await userRepository.findAllUsers()).length).toBe(1)
