85e3503f9a9a87e9a386ca0df5d6c221c70a7cb4
Marcelo Shirayama <caixadecorreiodomarcelo@gmail.com>
2022-07-12 18:53:23 -0300

    refactor: make frequently used variables global in user.spec

--- tests/entities/user.spec.ts
+++ tests/entities/user.spec.ts
@@ -3,10 +3,14 @@ import { InvalidPasswordError } from '@/entities/errors/invalid-password-error'
 import { User } from '@/entities/user'
 import { left } from '@/shared/either'
 
+const validEmail = 'valid@mail.com'
+const validPassword = 'valid_password_1'
+const invalidEmail = 'invalid_email'
+const invalidPasswordWithoutNumbers = 'invalid_password'
+const invalidPasswordWithTooFewCharacters = '123ab'
+
 describe('User domain entity', () => {
   it('Should not create user with invalid e-mail address', () => {
-    const invalidEmail = 'invalid_email'
-    const validPassword = 'valid_password_1'
     const error = User.create({ email: invalidEmail, password: validPassword })
 
     expect(error.isLeft()).toBe(true)
@@ -15,26 +19,30 @@ describe('User domain entity', () => {
 
   it('Should not create user with invalid password (no numbers)', () => {
     const validEmail = 'valid@mail.com'
-    const invalidPassword = 'invalid_password'
-    const error = User.create({ email: validEmail, password: invalidPassword })
+    const error = User.create({
+      email: validEmail,
+      password: invalidPasswordWithoutNumbers
+    })
 
     expect(error.isLeft()).toBe(true)
-    expect(error).toEqual(left(new InvalidPasswordError(invalidPassword)))
+    expect(error).toEqual(
+      left(new InvalidPasswordError(invalidPasswordWithoutNumbers))
+    )
   })
 
   it('Should not create user with invalid password (too few chars)', () => {
-    const validEmail = 'valid@mail.com'
-    const invalidPassword = '123ab'
-
-    const error = User.create({ email: validEmail, password: invalidPassword })
+    const error = User.create({
+      email: validEmail,
+      password: invalidPasswordWithTooFewCharacters
+    })
 
     expect(error.isLeft()).toBe(true)
-    expect(error).toEqual(left(new InvalidPasswordError(invalidPassword)))
+    expect(error).toEqual(
+      left(new InvalidPasswordError(invalidPasswordWithTooFewCharacters))
+    )
   })
 
   it('Should create user with valid data', () => {
-    const validEmail = 'any@mail.com'
-    const validPassword = 'valid_password_1'
     const user: User = User.create({
       email: validEmail,
       password: validPassword
