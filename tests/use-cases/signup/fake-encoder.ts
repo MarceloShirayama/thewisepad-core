8183b9cd743f133b0ad0f388b1db5c6a86a40d8e
Marcelo Shirayama <caixadecorreiodomarcelo@gmail.com>
2022-07-12 19:09:24 -0300

    refactor: make encode asynchronous

--- tests/use-cases/signup/fake-encoder.ts
+++ tests/use-cases/signup/fake-encoder.ts
@@ -1,7 +1,7 @@
 import { Encoder } from '@/use-cases/signup/ports/encoder'
 
 export class FakeEncoder implements Encoder {
-  encode(plain: string): Promise<string> {
-    return Promise.resolve(plain + 'ENCRYPTED')
+  public async encode(plain: string): Promise<string> {
+    return plain + 'ENCRYPTED'
   }
 }
