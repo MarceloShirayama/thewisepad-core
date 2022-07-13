e4572dfe5f55e1f45bba039ac5b2ec93acab9c31
Marcelo Shirayama <caixadecorreiodomarcelo@gmail.com>
2022-07-13 17:04:46 -0300

    chore: add jest coverage ignore

--- jest.config.js
+++ jest.config.js
@@ -11,5 +11,13 @@ module.exports = {
   moduleNameMapper: {
     '@/(.*)': '<rootDir>/src/$1',
     '@tests/(.*)': '<rootDir>/tests/$1'
-  }
+  },
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.{ts,tsx}',
+    '!<rootDir>/src/**/*.d.ts',
+    '!<rootDir>/src/**/either.{ts,tsx}',
+    '!<rootDir>/src/index.{ts,tsx}',
+    '!<rootDir>/src/types/*.{ts,tsx}',
+    '!<rootDir>/src/**/module-alias.{ts,tsx}'
+  ]
 }
