4e53c6d46151f0c4151918b0a908c8835a550cc6
Marcelo Shirayama <caixadecorreiodomarcelo@gmail.com>
2022-07-14 13:17:08 -0300

    test: fail to load notes for user without notes

--- tests/unit/use-cases/load-notes-for-user/load-notes-for-user.spec.ts
+++ tests/unit/use-cases/load-notes-for-user/load-notes-for-user.spec.ts
@@ -7,6 +7,7 @@ describe('Load notes for user use case', () => {
   const validTitle1 = 'my note'
   const validTitle2 = 'my second note'
   const validUserId = '0'
+  const userIdWithoutNotes = '1'
   const someContent = 'some content'
   const someOtherContent = 'some other content'
   const note1: NoteData = {
@@ -31,5 +32,13 @@ describe('Load notes for user use case', () => {
 
     expect(notes).toHaveLength(2)
     expect(notes).toEqual([note1, note2])
+  })
+
+  it('Should fail to load notes for user without notes', async () => {
+    const useCase = new LoadNotesForUser(noteRepositoryWithTwoNotes)
+
+    const notes: NoteData[] = await useCase.perform(userIdWithoutNotes)
+
+    expect(notes).toHaveLength(0)
   })
 })
