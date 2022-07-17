690b6e3533c31789f29ae334f93d305e3d621404
Marcelo Shirayama <caixadecorreiodomarcelo@gmail.com>
2022-07-17 18:04:23 -0300

    feat: handle internal server errors

--- src/controllers/sign-up.ts
+++ src/controllers/sign-up.ts
@@ -1,5 +1,5 @@
 import { HttpResponse } from '@/controllers/ports'
-import { badRequest, created, forbidden } from '@/controllers/util'
+import { badRequest, created, forbidden, serverError } from '@/controllers/util'
 import { UserData } from '@/use-cases/ports'
 import { SignUp } from '@/use-cases/sign-up'
 import { ExistingUserError } from '@/use-cases/sign-up/errors'
@@ -8,16 +8,20 @@ export class SignUpController {
   constructor(private readonly useCase: SignUp) {}
 
   async handle(request: UserData): Promise<HttpResponse> {
-    const response = await this.useCase.perform(request)
+    try {
+      const response = await this.useCase.perform(request)
 
-    if (response.isRight()) {
-      return created(response.value)
+      if (response.isRight()) {
+        return created(response.value)
+      }
+
+      if (response.isLeft() && response.value instanceof ExistingUserError) {
+        return forbidden(response.value)
+      }
+
+      return badRequest(response.value)
+    } catch (error: any) {
+      return serverError(error)
     }
-
-    if (response.isLeft() && response.value instanceof ExistingUserError) {
-      return forbidden(response.value)
-    }
-
-    return badRequest(response.value)
   }
 }
