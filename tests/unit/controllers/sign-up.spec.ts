fc05c70e6fcde51de52f54fb32a38ad11bd2f138
Marcelo Shirayama <caixadecorreiodomarcelo@gmail.com>
2022-07-17 17:15:56 -0300

    test: handle existing user request

--- tests/unit/controllers/sign-up.spec.ts
+++ tests/unit/controllers/sign-up.spec.ts
@@ -7,22 +7,42 @@ import { Encoder, UserData, UserRepository } from '@/use-cases/ports'
 import { SignUp } from '@/use-cases/sign-up'
 
 describe('Sign up controller', () => {
+  const emptyRepository: UserRepository = new InMemoryUserRepository([])
+
+  const encoder: Encoder = new FakeEncoder()
+
+  const SignUpUseCase: SignUp = new SignUp(emptyRepository, encoder)
+
+  const validUserSignUpData: UserData = UserDataBuilder.validUser().build()
+
+  const controller: SignUpController = new SignUpController(SignUpUseCase)
+
   it('Should return 200 and the registered user, if successful', async () => {
-    const emptyRepository: UserRepository = new InMemoryUserRepository([])
-
-    const encoder: Encoder = new FakeEncoder()
-
-    const useCase: SignUp = new SignUp(emptyRepository, encoder)
-
-    const validUserSignUpData: UserData = UserDataBuilder.validUser().build()
-
-    const controller: SignUpController = new SignUpController(useCase)
-
     const response: HttpResponse = await controller.handle(validUserSignUpData)
 
     const { statusCode, body } = response
 
     expect(statusCode).toBe(200)
-    expect(body).toEqual(validUserSignUpData)
+    expect(body).toEqual({
+      id: expect.any(String),
+      email: validUserSignUpData.email,
+      password: expect.any(String)
+    })
+  })
+
+  it('Should return 403 when trying to sign up existing user', async () => {
+    await controller.handle(validUserSignUpData)
+
+    const response: HttpResponse = await controller.handle(validUserSignUpData)
+
+    const { statusCode, body } = response
+    const error = body as Error
+
+    expect(statusCode).toBe(403)
+    expect(error.name).toBe('ExistingUserError')
+    expect(error.message).toBe(
+      `User ${validUserSignUpData.email} already registered`
+    )
+    expect(error.stack).toBeDefined()
   })
 })
