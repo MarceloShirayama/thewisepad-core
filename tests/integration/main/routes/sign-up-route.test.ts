c46c388678390c6bf632ec18c9e74d56a1566ea1
Marcelo Shirayama <caixadecorreiodomarcelo@gmail.com>
2022-07-18 18:34:06 -0300

    test: setup sign up route

---
+++ tests/integration/main/routes/sign-up-route.test.ts
@@ -0,0 +1,19 @@
+import request from 'supertest'
+
+import { app } from '@/main/config/app'
+
+describe('Register routes', () => {
+  it('Should return an account on success', async () => {
+    await request(app)
+      .post('/api/sign-up')
+      .send({
+        email: 'any@mail.com',
+        password: 'valid_password_123'
+      })
+      .expect(201, {
+        email: 'any@mail.com',
+        password: 'valid_password_123_ENCRYPTED',
+        id: '0'
+      })
+  })
+})
