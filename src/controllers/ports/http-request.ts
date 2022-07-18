b080173654b1f67f261e2fb66c4944cb238b10eb
Marcelo Shirayama <caixadecorreiodomarcelo@gmail.com>
2022-07-17 21:11:59 -0300

    feat: add generics types in request

--- src/controllers/ports/http-request.ts
+++ src/controllers/ports/http-request.ts
@@ -1,3 +1,3 @@
-export interface HttpRequest {
-  body?: any
+export interface HttpRequest<T = any> {
+  body?: T
 }
--- src/controllers/sign-up.ts
+++ src/controllers/sign-up.ts
@@ -1,16 +1,17 @@
 import { Controller, HttpRequest, HttpResponse } from '@/controllers/ports'
 import { badRequest, created, forbidden, serverError } from '@/controllers/util'
+import { UserData } from '@/use-cases/ports'
 import { SignUp } from '@/use-cases/sign-up'
 import { ExistingUserError } from '@/use-cases/sign-up/errors'
 
 export class SignUpController implements Controller {
   constructor(private readonly useCase: SignUp) {}
 
-  async handle(request: HttpRequest): Promise<HttpResponse> {
+  async handle(request: HttpRequest<UserData>): Promise<HttpResponse> {
     try {
       const response = await this.useCase.perform({
-        email: request.body.email,
-        password: request.body.password
+        email: request.body?.email as string,
+        password: request.body?.password as string
       })
 
       if (response.isRight()) {
