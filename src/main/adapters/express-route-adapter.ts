415086703b97c32558272f22227944dbe85c667f
Marcelo Shirayama <caixadecorreiodomarcelo@gmail.com>
2022-07-17 21:20:44 -0300

    feat: express adapter

---
+++ src/main/adapters/express-route-adapter.ts
@@ -0,0 +1,13 @@
+import { Controller, HttpRequest } from '@/controllers/ports'
+import { Request, Response } from 'express'
+
+export function adaptRoute(controller: Controller) {
+  return async (req: Request, res: Response) => {
+    const httpRequest: HttpRequest = {
+      body: req.body
+    }
+    const httpResponse = await controller.handle(httpRequest)
+
+    res.status(httpResponse.statusCode).json(httpResponse.body)
+  }
+}
