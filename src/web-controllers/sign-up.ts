62cc98cd8bb036669ce164dfce107a105abce214
Marcelo Shirayama <caixadecorreiodomarcelo@gmail.com>
2022-07-18 16:20:31 -0300

    feat: correctly handles missing parameters on request

--- src/web-controllers/sign-up.ts
+++ src/web-controllers/sign-up.ts
@@ -11,12 +11,23 @@ import {
   forbidden,
   serverError
 } from '@/web-controllers/util'
+import { MissingParamError } from './errors'
 
 export class SignUpController implements WebController {
   constructor(private readonly useCase: UseCase) {}
 
   async handle(request: HttpRequest<UserData>): Promise<HttpResponse> {
     try {
+      const requestParamsRequired = ['email', 'password']
+      const requestParam = Object.keys(request.body!)
+      const missingParam = requestParamsRequired.filter(
+        (param) => !requestParam.includes(param)
+      )
+
+      if (missingParam.length > 0) {
+        return badRequest(new MissingParamError(missingParam.join(', ')))
+      }
+
       const response = await this.useCase.perform({
         email: request.body?.email as string,
         password: request.body?.password as string
