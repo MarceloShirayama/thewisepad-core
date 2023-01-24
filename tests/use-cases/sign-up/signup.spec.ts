b7ea07e72ac58abe9e939bd070dba3dab4f3baf3
Marcelo Shirayama <caixadecorreiodomarcelo@gmail.com>
2023-01-24 19:31:02 -0300

    test: prevent user from signing up with invalid email or password

--- tests/use-cases/sign-up/signup.spec.ts
+++ tests/use-cases/sign-up/signup.spec.ts
@@ -5,10 +5,15 @@ import { SignUp } from "@/use-cases/sign-up/signup";
 import { InMemoryUserRepository } from "../in-memory-user-repository";
 import { FakeEncoder } from "./fake-encoder";
 import { ExistingUserError } from "@/use-cases/sign-up/errors/existing-user-error";
+import { InvalidPasswordError } from "@/entities/errors/invalid-password-error";
+import { InvalidEmailError } from "@/entities/errors/invalid-email-error";
 
 describe("SignUp use case", () => {
   const validEmail = "any@mail.com";
   const validPassword = "1valid_password";
+
+  const invalidEmail = "invalid_email";
+  const invalidPassword = "1abc";
 
   const userSignupRequest: UserData = {
     email: validEmail,
@@ -48,5 +53,39 @@ describe("SignUp use case", () => {
     const error = await signUpUseCase.perform(userSignupRequest);
 
     expect(error.value).toEqual(new ExistingUserError(userSignupRequest));
+  });
+
+  test("Should not sign up user with invalid password", async () => {
+    const userSignupRequestWithInvalidPassword = {
+      email: validEmail,
+      password: invalidPassword,
+    };
+
+    const { signUpUseCase } = makeSut();
+
+    const error = await signUpUseCase.perform(
+      userSignupRequestWithInvalidPassword
+    );
+
+    expect(error.value).toEqual(
+      new InvalidPasswordError(userSignupRequestWithInvalidPassword.password)
+    );
+  });
+
+  test("Should not sign up user with invalid email", async () => {
+    const userSignupRequestWithInvalidPassword = {
+      email: invalidEmail,
+      password: validPassword,
+    };
+
+    const { signUpUseCase } = makeSut();
+
+    const error = await signUpUseCase.perform(
+      userSignupRequestWithInvalidPassword
+    );
+
+    expect(error.value).toEqual(
+      new InvalidEmailError(userSignupRequestWithInvalidPassword.email)
+    );
   });
 });
