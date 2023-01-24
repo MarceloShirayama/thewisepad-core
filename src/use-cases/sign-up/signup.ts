2642a3b28c4082b45bba8a26f3677584e156059b
Marcelo Shirayama <caixadecorreiodomarcelo@gmail.com>
2023-01-24 19:23:35 -0300

    feat: prevent user from signing up with invalid email or password

--- src/use-cases/sign-up/signup.ts
+++ src/use-cases/sign-up/signup.ts
@@ -1,3 +1,6 @@
+import { InvalidEmailError } from "@/entities/errors/invalid-email-error";
+import { InvalidPasswordError } from "@/entities/errors/invalid-password-error";
+import { User } from "@/entities/user";
 import { UserData } from "@/entities/user-data";
 import { Either, left, right } from "@/shared/either";
 import { UserRepository } from "../ports/user-repository";
@@ -12,7 +15,16 @@ export class SignUp {
 
   async perform(
     userSignupRequest: UserData
-  ): Promise<Either<ExistingUserError, UserData>> {
+  ): Promise<
+    Either<
+      ExistingUserError | InvalidEmailError | InvalidPasswordError,
+      UserData
+    >
+  > {
+    const userOrError = User.create(userSignupRequest);
+
+    if (userOrError.isLeft()) return left(userOrError.value);
+
     const userAlreadyExists = await this.userRepository.findUserByEmail(
       userSignupRequest.email
     );
