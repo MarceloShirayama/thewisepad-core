317baa0625de75d77e483303af3f45bae2273cbc
Marcelo Shirayama <caixadecorreiodomarcelo@gmail.com>
2023-01-24 18:31:23 -0300

    refactor: simplify code

--- tests/entities/user.spec.ts
+++ tests/entities/user.spec.ts
@@ -6,26 +6,25 @@ import { left } from "@/shared/either";
 import { InvalidPasswordError } from "@/entities/errors/invalid-password-error";
 
 describe("User domain entity", () => {
+  const validEmail = "any@mail.com";
+  const invalidEmail = "invalid_email";
+  const validPassword = "1valid_password";
+  const invalidPassword = "invalid_password";
+
   test("should not create user with invalid e-mail address", () => {
-    const invalidEmail = "invalid_email";
-    const validPassword = "1valid_password";
     const error = User.create({ email: invalidEmail, password: validPassword });
 
     expect(error).toEqual(left(new InvalidEmailError(invalidEmail)));
   });
 
   test("should create user with valid data", () => {
-    const validEmail = "any@mail.com";
-    const validPassword = "1valid_password";
     const user = User.create({ email: validEmail, password: validPassword })
       .value as User;
+
     expect(user.email.value).toEqual(validEmail);
   });
 
   test("should not create user with invalid password (no numbers)", () => {
-    const validEmail = "any@mail.com";
-    const invalidPassword = "invalid_password";
-
     const error = User.create({ email: validEmail, password: invalidPassword });
 
     expect(error).toEqual(left(new InvalidPasswordError(invalidPassword)));
