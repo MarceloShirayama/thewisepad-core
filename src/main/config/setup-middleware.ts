260d96da89bc88f08c802b9422046254bd30f006
Marcelo Shirayama <caixadecorreiodomarcelo@gmail.com>
2023-01-27 12:27:15 -0300

    feat: enable json content type middleware and tests

--- src/main/config/setup-middleware.ts
+++ src/main/config/setup-middleware.ts
@@ -1,7 +1,8 @@
 import { Express } from "express";
-import { bodyParser, cors } from "@/main/middleware";
+import { bodyParser, contentType, cors } from "@/main/middleware";
 
 export function setupMIddleware(app: Express): void {
   app.use(bodyParser);
   app.use(cors);
+  app.use(contentType);
 }
