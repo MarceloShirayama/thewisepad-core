d7f7a4d97e746e20654e42387061adf54934fe70
Marcelo Shirayama <caixadecorreiodomarcelo@gmail.com>
2023-01-27 06:47:03 -0300

    refactor: use http request on controller and tests

--- src/presentation/controllers/sign-up.ts
+++ src/presentation/controllers/sign-up.ts
@@ -1,15 +1,18 @@
 import { UserData } from "@/use-cases/ports";
 import { SignUp } from "@/use-cases/sign-up";
 import { ExistingUserError } from "@/use-cases/sign-up/errors";
-import { Controller, HttpResponse } from "./ports";
+import { Controller, HttpRequest, HttpResponse } from "./ports";
 import { badRequest, created, forbidden, serverError } from "./util";
 
 export class SignUpController implements Controller {
   constructor(private readonly useCase: SignUp) {}
 
-  async handle(request: UserData): Promise<HttpResponse> {
+  async handle(request: HttpRequest<UserData>): Promise<HttpResponse> {
     try {
-      const response = await this.useCase.perform(request);
+      const response = await this.useCase.perform({
+        email: request.body.email,
+        password: request.body.password,
+      });
 
       if (response.isRight()) return created(response.value);
 
--- tests/presentation/controllers/sign-up.spec.ts
+++ tests/presentation/controllers/sign-up.spec.ts
@@ -7,16 +7,41 @@ import { FakeEncoder } from "tests/doubles/encoder";
 import { InMemoryUserRepository } from "tests/doubles/repositories";
 import { ExistingUserError } from "@/use-cases/sign-up/errors";
 import { InvalidEmailError, InvalidPasswordError } from "@/entities/errors";
+import { HttpRequest } from "@/presentation/controllers/ports";
+import { UserData } from "@/use-cases/ports";
 
 describe("Sign up controller", () => {
   function makeSut() {
     const validUser = UserBuilder.createUser().build();
+
+    const validUserRequest: HttpRequest<UserData> = {
+      body: {
+        email: validUser.email,
+        password: validUser.password,
+      },
+    };
+
     const userWithInvalidEmail = UserBuilder.createUser()
       .withInvalidEmail()
       .build();
+
+    const userWithInvalidEmailRequest: HttpRequest<UserData> = {
+      body: {
+        email: userWithInvalidEmail.email,
+        password: userWithInvalidEmail.password,
+      },
+    };
+
     const userWithInvalidPassword = UserBuilder.createUser()
       .withPasswordWithTooFewChars()
       .build();
+
+    const userWithInvalidPasswordRequest: HttpRequest<UserData> = {
+      body: {
+        email: userWithInvalidPassword.email,
+        password: userWithInvalidPassword.password,
+      },
+    };
 
     const emptyUserRepository = new InMemoryUserRepository([]);
     const encoder = new FakeEncoder();
@@ -27,55 +52,55 @@ describe("Sign up controller", () => {
 
     return {
       controller,
-      validUser,
-      userWithInvalidEmail,
-      userWithInvalidPassword,
       useCase,
+      validUserRequest,
+      userWithInvalidPasswordRequest,
+      userWithInvalidEmailRequest,
     };
   }
 
-  test("Should return 200 and registered user when user is successfully signed up", async () => {
-    const { controller, validUser } = makeSut();
+  test("Should return 201 and registered user when user is successfully signed up", async () => {
+    const { controller, validUserRequest } = makeSut();
 
-    const response = await controller.handle(validUser);
+    const response = await controller.handle(validUserRequest);
 
     expect(response.statusCode).toBe(201);
     expect(response.body).toHaveProperty("id");
     expect(response.body).toHaveProperty("password");
-    expect(response.body).toHaveProperty("email", validUser.email);
+    expect(response.body).toHaveProperty("email", validUserRequest.body.email);
   });
 
   test("Should return 403 when trying to sign up existing user", async () => {
-    const { controller, validUser } = makeSut();
+    const { controller, validUserRequest } = makeSut();
 
-    await controller.handle(validUser);
+    await controller.handle(validUserRequest);
 
-    const response = await controller.handle(validUser);
+    const response = await controller.handle(validUserRequest);
 
     expect(response.statusCode).toBe(403);
     expect(response.body).toBeInstanceOf(ExistingUserError);
   });
 
   test("Should return 400 when trying to sign up user with invalid email", async () => {
-    const { controller, userWithInvalidEmail } = makeSut();
+    const { controller, userWithInvalidEmailRequest } = makeSut();
 
-    const response = await controller.handle(userWithInvalidEmail);
+    const response = await controller.handle(userWithInvalidEmailRequest);
 
     expect(response.statusCode).toBe(400);
     expect(response.body).toBeInstanceOf(InvalidEmailError);
   });
 
   test("Should return 400 when trying to sign up user with invalid password", async () => {
-    const { controller, userWithInvalidPassword } = makeSut();
+    const { controller, userWithInvalidPasswordRequest } = makeSut();
 
-    const response = await controller.handle(userWithInvalidPassword);
+    const response = await controller.handle(userWithInvalidPasswordRequest);
 
     expect(response.statusCode).toBe(400);
     expect(response.body).toBeInstanceOf(InvalidPasswordError);
   });
 
   test("Should return 500 if an error is raised internally", async () => {
-    const { useCase, validUser } = makeSut();
+    const { useCase, validUserRequest } = makeSut();
 
     const originalPerform = SignUp.prototype.perform;
     SignUp.prototype.perform = () => {
@@ -84,7 +109,7 @@ describe("Sign up controller", () => {
 
     const controller = new SignUpController(useCase);
 
-    const response = await controller.handle(validUser);
+    const response = await controller.handle(validUserRequest);
 
     expect(response.statusCode).toBe(500);
     expect(response.body).toBeInstanceOf(Error);
