1a7d6e9c1690c1073ccf893c8809775861d34cde
Marcelo Shirayama <caixadecorreiodomarcelo@gmail.com>
2023-02-07 12:30:04 -0300

    refactor: make bcrypt rounds come from environment variable

--- src/external/encoder/bcrypt-encoder.ts
+++ src/external/encoder/bcrypt-encoder.ts
@@ -1,11 +1,12 @@
 import bcrypt from "bcrypt";
+import { env } from "../../main/config/environment";
 
 import { Encoder } from "../../use-cases/ports";
 
 export class BcryptEncoder implements Encoder {
   private readonly rounds: number;
 
-  constructor(rounds: number = 10) {
+  constructor(rounds: number = env.BCRYPT_ROUNDS) {
     this.rounds = rounds;
   }
 
--- src/main/config/environment.ts
+++ src/main/config/environment.ts
@@ -4,4 +4,5 @@ config();
 
 export const env = {
   JWT_SECRET: process.env.JWT_SECRET as string,
+  BCRYPT_ROUNDS: Number(process.env.BCRYPT_ROUNDS),
 };
