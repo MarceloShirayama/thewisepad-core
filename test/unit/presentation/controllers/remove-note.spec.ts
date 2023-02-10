cea5ba37ab3dcba579501daa963c9cd85226322d
Marcelo Shirayama <caixadecorreiodomarcelo@gmail.com>
2023-02-10 15:29:00 -0300

    RemoveNoteController should return 400 in an attempt to remove the non-existing note

--- test/unit/presentation/controllers/remove-note.spec.ts
+++ test/unit/presentation/controllers/remove-note.spec.ts
@@ -1,11 +1,15 @@
 import { RemoveNoteController } from "src/presentation/controllers/remove-note";
 import { RemoveNote } from "src/use-cases/remove-note";
+import { NoExistentNoteError } from "src/use-cases/remove-note/errors";
 import { NoteBuilder } from "test/builders/note-builder";
 import { InMemoryNoteRepository } from "test/doubles/repositories";
 
 describe("Remove note controller", () => {
-  it("Should return 200 if successfully removing note", async () => {
+  function makeSut() {
     const validNote = NoteBuilder.createNote().build();
+    const anotherValidNote = NoteBuilder.createNote()
+      .withDifferentTitleAndId()
+      .build();
 
     const noteRepositoryWithOneNote = new InMemoryNoteRepository([validNote]);
 
@@ -13,11 +17,28 @@ describe("Remove note controller", () => {
 
     const controller = new RemoveNoteController(useCase);
 
+    return { validNote, anotherValidNote, controller };
+  }
+
+  it("Should return 200 if successfully removing note", async () => {
+    const { validNote, controller } = makeSut();
+
     const response = await controller.handle({
       body: { noteId: validNote.id },
     });
 
     expect(response.statusCode).toBe(200);
     expect(response.body).toBe(validNote);
+  });
+
+  it("Should return 400 in an attempt to remove the non-existing note", async () => {
+    const { anotherValidNote, controller } = makeSut();
+
+    const response = await controller.handle({
+      body: { noteId: anotherValidNote.id },
+    });
+
+    expect(response.statusCode).toBe(400);
+    expect(response.body).toBeInstanceOf(NoExistentNoteError);
   });
 });
